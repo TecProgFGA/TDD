0bbebccdd8d56a2e4457467522b8a27334d7b94c
Natanael Filho <filhonatanael01@gmail.com>
2022-12-21 12:44:56 -0300

    fix: testes cadastro rendimento

--- calculadoraIRPF/irpf/src/test/java/unb/tecprog/CadastroRendimentosTest.java
+++ calculadoraIRPF/irpf/src/test/java/unb/tecprog/CadastroRendimentosTest.java
@@ -17,23 +17,23 @@ public class CadastroRendimentosTest
 
     @Test
     public void T01_CadastroUmRendimento() {
-        simuladorIRPF.cadastrarRendimentos("Salario", 16000.0);
+        simuladorIRPF.cadastrarRendimento(new Rendimento("Salario", 16000.0));
         assertEquals(16000.0, simuladorIRPF.getTotalRendimentos(), 0.0);
     }
 
     @Test
     public void T02_CadastroDoisRendimento() {
-        simuladorIRPF.cadastrarRendimentos("Salario", 10000.0);
-        simuladorIRPF.cadastrarRendimentos("Aluguel", 2000.0);
+        simuladorIRPF.cadastrarRendimento(new Rendimento("Salario", 10000.0));
+        simuladorIRPF.cadastrarRendimento(new Rendimento("Aluguel", 2000.0));
         assertEquals(12000.0, simuladorIRPF.getTotalRendimentos(), 0.0);
     }
 
-   @Test
-   public void T03_CadastroTresRendimento() {
-       simuladorIRPF.cadastrarRendimentos("Salario Total", 12000.0);
-       simuladorIRPF.cadastrarRendimentos("Aluguel", 2000.0);
-       simuladorIRPF.cadastrarRendimentos("Rendimento de dividendos", 1000.0);
-       assertEquals(15000.0, simuladorIRPF.getTotalRendimentos(), 0.0);
-   }
+    @Test
+    public void T03_CadastroTresRendimento() {
+        simuladorIRPF.cadastrarRendimento(new Rendimento("Salario Total", 12000.0));
+        simuladorIRPF.cadastrarRendimento(new Rendimento("Aluguel", 2000.0));
+        simuladorIRPF.cadastrarRendimento(new Rendimento("Rendimento de dividendos", 1000.0));
+        assertEquals(15000.0, simuladorIRPF.getTotalRendimentos(), 0.0);
+    }
 
 }
