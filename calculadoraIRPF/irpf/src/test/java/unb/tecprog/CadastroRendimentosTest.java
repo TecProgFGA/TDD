bd90b14ae07c3efd9570d95cd185afab490c1539
art1505 <arthurtalles@yahoo.com.br>
2022-12-21 17:38:00 -0300

    "Duplicação: adicionando esta técnica à classe de cadastro de rendimentos
    Co-Authored-By: Davi Matheus <54643148+DaviMatheus@users.noreply.github.com>
    Co-Authored-By: Natanael Filho <filhonatanael01@gmail.com>
    Co-authored-by: EduardoGurgel<eduardo-gurgel@hotmail.com>"

--- calculadoraIRPF/irpf/src/test/java/unb/tecprog/CadastroRendimentosTest.java
+++ calculadoraIRPF/irpf/src/test/java/unb/tecprog/CadastroRendimentosTest.java
@@ -22,10 +22,24 @@ public class CadastroRendimentosTest
     }
 
     @Test
+    public void T01_CadastroUmRendimentoDuplicado() {
+        simuladorIRPF.cadastrarRendimento("Salario", 20000.0);
+        assertEquals(20000.0, simuladorIRPF.getTotalRendimentos(), 0.0);
+    }
+
+
+    @Test
     public void T02_CadastroDoisRendimento() {
         simuladorIRPF.cadastrarRendimento("Salario", 10000.0);
         simuladorIRPF.cadastrarRendimento("Aluguel", 2000.0);
         assertEquals(12000.0, simuladorIRPF.getTotalRendimentos(), 0.0);
+    }
+
+    @Test
+    public void T02_CadastroDoisRendimentoDuplicado() {
+        simuladorIRPF.cadastrarRendimento("Salario", 18000.0);
+        simuladorIRPF.cadastrarRendimento("Aluguel", 2000.0);
+        assertEquals(20000.0, simuladorIRPF.getTotalRendimentos(), 0.0);
     }
 
     @Test
@@ -34,6 +48,14 @@ public class CadastroRendimentosTest
         simuladorIRPF.cadastrarRendimento("Aluguel", 2000.0);
         simuladorIRPF.cadastrarRendimento("Rendimento de dividendos", 1000.0);
         assertEquals(15000.0, simuladorIRPF.getTotalRendimentos(), 0.0);
+    }
+
+    @Test
+    public void T03_CadastroTresRendimentoDuplicado() {
+        simuladorIRPF.cadastrarRendimento("Salario Total", 12000.0);
+        simuladorIRPF.cadastrarRendimento("Aluguel", 5000.0);
+        simuladorIRPF.cadastrarRendimento("Rendimento de dividendos", 3000.0);
+        assertEquals(20000.0, simuladorIRPF.getTotalRendimentos(), 0.0);
     }
 
 }
